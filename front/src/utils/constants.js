bc345fda41f971e6fd0734bdeef08e2efde04dce
Lily Perera <lilycapetillo86@gmail.com>
2025-05-24 00:30:17 +0000

    fix (front) Validate other slash in constant setting

--- front/src/utils/constants.js
+++ front/src/utils/constants.js
@@ -6,10 +6,10 @@
 export const API_ENDPOINTS = {
   VIDEOS: '/videos/',
   VIDEO: (id) => `/videos/${id}`,
-  VIDEO_STREAM: (id) => `/videos/${id}/stream`,
-  PLAYLISTS: '/playlists',
+  VIDEO_STREAM: (id) => `/videos/${id}/stream/`,
+  PLAYLISTS: '/playlists/',
   PLAYLIST: (id) => `/playlists/${id}`,
-  PLAYLIST_VIDEOS: (id) => `/playlists/${id}/videos`,
+  PLAYLIST_VIDEOS: (id) => `/playlists/${id}/videos/`,
 };
 
 // Application routes
